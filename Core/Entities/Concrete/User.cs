0e367de0210c92dedbeee0f0dd7159f6df256af2
Senol <senoltr@yandex.com>
2024-01-19 20:49:56 +0300

    Core katmanına user nesnesi oluşturuldu.

---
+++ Core/Entities/Concrete/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities.Concrete
+{
+    public class User:BaseEntity
+    {
+        public Guid UserId { get; set; }
+        public Guid FacultyId { get; set; }
+        public Guid DepartmentId { get; set; }
+        public string SchoolNumber { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public byte[] PasswordHash { get; set; }
+    }
+}
